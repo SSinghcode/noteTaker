a93b5be633c7d34f404f48cde7f5fc09fd9d74f4
navjot turna <shamsherturna101@gmail.com>
2021-10-28 09:42:45 -0400

    inde

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,11 @@
 const express = require("express");
-const path = require("path");
+const notesRouter = require("./notes");
 
-// app declaration
+// Declaring App
 const app = express();
 
-//ports
-const PORT = process.env.PORT || 3003;
-
-// 
-const route = require("./routes/index.js");
-
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", route);
+app.use("/notes", notesRouter);
 
-// index route
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-//notes page api
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-
-// wildcard error
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/404.html"));
-});
-
-// Listening on Port
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT} 🚀`));
+// Exporting app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// require
 const express = require("express");
 const path = require("path");
 
